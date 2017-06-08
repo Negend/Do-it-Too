fc6fbb36ac418e74f37ed749c695476691ae163d
Nneji Nwakamma <nnwakamma@spartaglobal.co>
2017-06-08 16:41:57 +0100

    timestamp on each image name to keep it unique

--- do-it-too/controllers/toos-controller.rb
+++ do-it-too/controllers/toos-controller.rb
@@ -29,30 +29,28 @@ class ToosController < Sinatra::Base
 	end
 
   post '/' do  
-  	File.open('public/uploads/' + params[:tfile][:filename], "w") do |f|
-    	f.write(params[:tfile][:tempfile].read)
-	  end
-	  @file = params[:tfile][:filename]
-	  too_file = "uploads/#{@file}"
-
     # too_file = params[:too_file]
-  # in future do it id will be from database or page. id = params[:id] for now = 1 
- # user specific posting(:ID represents do:it page)
-  # post '/:id' do   
+    # in future do it id will be from database or page. id = params[:id] for now = 1 
+    # user specific posting(:ID represents do:it page)
+    # post '/:id' do
+    rating = params[:rating]
+    @file = params[:tfile][:filename]   
     do_it_id = 1
     username = params[:username]
-   	toos = Too.all
    	# check if theyve posted before if not, add them as a tooder
    	unregistered = true
+   	toos = Too.all
    	toos.each {|todo| unregistered = false if username == todo.tooder}
    	if unregistered
    		Too.register_tooder username  	
-   	end
+   	end  	
 		user_id = Too.find_user_id username
-    rating = params[:rating]
+    # upload
+    stamp = Time.new.to_f.to_s.gsub('.','')
+    too_file = "uploads/#{stamp + @file }"
     Too.save do_it_id,user_id,too_file
-    @uploaded_too =Too.show_last_too
-    File.open('public/uploads/' + @uploaded_too.id + params[:tfile][:filename], "w") do |f|
+    # @uploaded_too =Too.show_last_too
+    File.open('public/uploads/'+stamp+ params[:tfile][:filename], "w") do |f|
     	f.write(params[:tfile][:tempfile].read)
 	  end  
     redirect "/"    
