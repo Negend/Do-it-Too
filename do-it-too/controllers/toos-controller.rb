130ba5222a52ed502e3a4bd69daef908bbbd339f
Nneji Nwakamma <nnwakamma@spartaglobal.co>
2017-06-08 05:57:08 +0100

    posting new content works

--- do-it-too/controllers/toos-controller.rb
+++ do-it-too/controllers/toos-controller.rb
@@ -29,17 +29,17 @@ class ToosController < Sinatra::Base
   end
     
   post '/' do   
-    tool_file = params[:tool_file]
+    too_file = params[:tool_file]
   # in future do it id will be from database or page. id = params[:id] for now = 1 
  # user specific posting(:ID represents do:it page)
   # post '/:id' do   
     do_it_id = 1
-    username = params[username]
-   	id = Too.find_user_id username
-   	if id
+    username = params[:username]
+   	user_id = Too.find_user_id username.to_s
+   	if user_id
    	else
    		Too.register_tooder username
-			id = Too.find_user_id username
+			user_id = Too.find_user_id username
    	end
     rating = params[:rating]
     Too.save do_it_id,user_id,too_file  
@@ -54,7 +54,7 @@ class ToosController < Sinatra::Base
   	'put'      
    	id = params[:id].to_i
     username = params[:username]
-    tool_file = params[:tool_file]
+    too_file = params[:tool_file]
     Post.update id,title,body
     redirect "/"
   end
--- do-it-too/models/too.rb
+++ do-it-too/models/too.rb
@@ -1,5 +1,5 @@
 class Too
-	attr_accessor :id, :user_id, :do_it_id, :too_file, :rating
+	attr_accessor :id, :user_id, :do_it_id, :too_file, :rating, :tooder, :date
 	
 	def self.open_connection
 		PG.connect(dbname: "do_it_too")
@@ -8,10 +8,12 @@ class Too
 	def self.hydrate too_data
     too = Too.new
     too.id = too_data['id']
+    too.date = too_data['join_date']
     too.user_id = too_data['user_id']
     too.too_file = too_data['too_file']
     too.do_it_id = too_data['do_it_id']
     too.rating = too_data['rating']
+    too.tooder = too_data['username']
     too
 	end
 
@@ -30,15 +32,16 @@ class Too
 	
 
 
-	def self.save do_it_id,user_id,tool_file
+	def self.save do_it_id,user_id,too_file
 		conn = self.open_connection
-		sql = "INSERT INTO testoos (user_id ,do_it_id ,too_file) VALUES (#{user_id},#{do_it_id},#{too_file})"
+		sql = "INSERT INTO testoos (user_id ,do_it_id ,too_file) VALUES (#{user_id},#{do_it_id},'#{too_file}')"
 		conn.exec(sql)
 	end
 
 	def self.find_user_id username
 		conn = self.open_connection
-		sql = "SELECT id FROM members WHERE username = #{username}"
+		sql = "SELECT id FROM members WHERE username = '#{username}'"
+		# SELECT id FROM members WHERE username = 'gyro'
 		result = conn.exec(sql)
 		id = result[0]['id'].to_i
 		id
@@ -47,16 +50,16 @@ class Too
 
 	def self.find_by_id id
 		conn = self.open_connection
-		sql = "SELECT  id, user_id, do_it_id, too_file, rating FROM testoos WHERE id = #{id}"
+		sql = "SELECT  t.id, user_id, do_it_id, too_file, rating, username, join_date FROM testoos t INNER JOIN members m ON t.user_id = m.id WHERE t.id = #{id}"
 		result = conn.exec(sql)
-    post = self.hydrate result[0]
-    post
+    too = self.hydrate result.first
+    too
 	end
 
 	def self.find_by_tooder username
 		self.find_user_id username
 		conn = self.open_connection
-		sql = "SELECT  id, user_id, do_it_id, too_file, rating FROM testoos WHERE id = #{user_id}"
+		sql = "SELECT  id,  user_id, do_it_id, too_file, rating FROM testoos WHERE id = #{user_id}"
 		results = conn.exec(sql)
 	   	toos = results.map do |tuple| 
 	        self.hydrate tuple
