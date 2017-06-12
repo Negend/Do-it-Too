71c2ba1e015a81fb26d66537bf1793f040ebaf0d
Nneji Nwakamma <nnwakamma@spartaglobal.co>
2017-06-12 11:14:37 +0100

    created a template styling for each too show page

--- do-it-too/public/index.js
+++ do-it-too/public/index.js
@@ -1,1 +1,114 @@
+$(loaded)
+function loaded (){
 console.log('jimini cricket')
+
+prettyStars()
+
+
+
+
+
+
+
+// function prettyStar (){
+// 	$('.rate').each(function(i,event){		
+// 		for(var x = 0; x = i; x++){
+// 		$($('.rate')[x]).hover(
+									
+// 				function () {
+// 				  $(this).css({"color":"red"})
+// 				}, 	
+// 				function () {
+// 				  $(this).css({"color":"blue"})
+// 				}
+// 			)
+// 		}		
+// 	})
+// }
+
+
+// function prettyStar (){
+// 	$('.rate').each(function(i,event){				
+// 		$(this).hover(
+									
+// 				function () {
+
+// 				  $(this).css({"color":"red"})
+// 				}, 	
+// 				function () {
+// 				  $(this).css({"color":"blue"})
+// 				}
+// 			)
+// 		}		
+// 	})
+// }
+
+function prettyStars (){
+	$('.rate').each(function(index,event){				
+		$(this).click(function(event){
+			$('.rate').each(function(index,event){
+				this.css({'display':'inline-block'})
+			})
+		}) 											
+	})
+}
+
+
+function prettyStar (){
+	$('.rate').each(function(index,event){				
+		$(this).hover(									
+			function(){
+			},
+			function(){} 					
+		)				
+	})
+}
+
+
+
+
+
+
+
+
+function starChange(color,i){
+	for(var x = 0; x = 3; x++){
+		$($('.rate')[x]).css({"color" : color})	
+	}
+}	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
