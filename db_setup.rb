e192015f07a7cec2e16ba8189d1e7d13572e575b
Nneji Nwakamma <nnwakamma@spartaglobal.co>
2017-06-13 12:59:50 +0100

    heroku

--- db_setup.rb
+++ db_setup.rb
@@ -8,16 +8,16 @@ class DatabaseSetup
             user: ENV['DB_USER'],
             password: ENV['DB_PASSWORD']
         })
-    end  postgres://tnoxaygjyosthg:b6a0bb49b0e1619bde26ea9f7edcc84b410fc69fa0f944fe6633947102dc2513@ec2-50-19-218-160.compute-1.amazonaws.com:5432/d613j0u3ts58e6
+    end  
     def self.go
         conn = self.open_connection
         sql = "CREATE TABLE testoos (
             id serial NOT NULL ,
             user_id INTEGER NOT NULL,
-            do_it_id  VARCHAR(250) NOT NULL,
-            ,rating TEXT
+            do_it_id  VARCHAR(250) NOT NULL,            
+            rating TEXT
         ); 
-        CRREATE TABLE members(
+        CREATE TABLE members(
             id serial NOT NULL,
             username varchar(50) NOT NULL,
             join_date DATE NOT NULL,
