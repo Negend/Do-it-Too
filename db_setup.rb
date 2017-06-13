46b3a93162b7933cfe17905f9033b8267fbe0b6c
Nneji Nwakamma <nnwakamma@spartaglobal.co>
2017-06-14 00:56:06 +0100

    remove heroku seed

--- db_setup.rb
+++ db_setup.rb
@@ -11,15 +11,9 @@ class DatabaseSetup
     end  
     def self.go
         conn = self.open_connection
-        sql = "drop table testoos; create table testoos (
-        id              serial not null,
-        do_it_id        integer not null,
-        user_id         integer not null,
-        too_file        varchar(200),
-        rating          varchar(30)    
-        );
-        insert into testoos (user_id, do_it_id,too_file,rating) values(0,1,'race.jpg','3,5');
-            insert into members (id,username,join_date,password) values(0,'tester',Current_timestamp,'hi');"
+        sql = "
+        delete from testoos where id = 0;
+            delete from members where id = 0;"
         conn.exec(sql)
     end
 end
